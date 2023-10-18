34951d19ed72cd8dc8bf438c5f76215ace2c4175
gerantonyk <ger.antonyk@gmail.com>
2023-10-18 02:40:25 +0100

    controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import { getTokensWithPrice } from './controllers/getTokensWithPrice';
 dotenv.config();
 
 const app: Application = express();
@@ -9,6 +10,7 @@ app.get('/health', (req: Request, res: Response) => {
   res.send('server is up and running');
 });
 
+app.get('/tokens', getTokensWithPrice);
 app.listen(port, () => {
   console.log(`Server is running at port:${port}`);
 });
---
+++ src/controllers/getTokensWithPrice.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { Provider } from '../providers/ankrProvider';
+import { GetTokensWithPriceDto } from '../dtos/getTokensWithPrice.dto';
+import { Blockchain } from '@ankr.com/ankr.js';
+
+const provider = new Provider();
+
+export const getTokensWithPrice = async (req: Request, res: Response) => {
+  console.log(req.query);
+  const tokenRequest: GetTokensWithPriceDto = {
+    walletAddress: req.query.walletAddress as string,
+    blockchain: (req.query.blockchain || 'eth') as Blockchain,
+    contractAddress: req.query.contractAddress as string | undefined,
+  };
+
+  if (!tokenRequest.walletAddress) {
+    return res.status(400).send('walletAddress is required');
+  }
+
+  try {
+    const tokens = await provider.getTokensWithPrice(tokenRequest); // Usa el token y address según sea necesario
+    console.log(tokens);
+    res.json({ tokens });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
